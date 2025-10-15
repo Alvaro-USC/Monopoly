76aed19c54f716416e5e47f69d03e2a4a5e67ed3
Álvaro <alvaro.schwiedop@rai.usc.es>
2025-10-15 14:20:30 +0200

    Delete monopoly/CasillaEspecial.java

--- monopoly/CasillaEspecial.java
+++
@@ -1,7 +0,0 @@
-package monopoly;
-public class CasillaEspecial {
-    public static final String SALIDA = "Salida";
-    public static final String CARCEL = "Carcel";
-    public static final String IR_CARCEL = "IrCarcel";
-    public static final String PARKING = "Parking";
-}
