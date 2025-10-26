c27ef39c86758b72ebfcdae80f44281479ec6a65
Alvaro <alvaro.schwiedop@rai.usc.es>
2025-10-26 19:00:10 +0100

    Cantidad dinero a pagar en Carcel

--- monopoly/Carcel.java
+++ monopoly/Carcel.java
@@ -21,7 +21,7 @@ public class Carcel extends Casilla {
     @Override
     public String infoCasilla() {
         String info = "{ \n tipo: " + getTipo();
-        info += ", \n salir: 500.000";
+        info += ", \n salir: 500.000"; // Cantidad a pagar para salir de la cárcel
         String jugs = "";
         for (Avatar a : getAvatares()) {
             jugs += "[" + a.getJugador().getNombre() + "," + a.getJugador().getTiradasCarcel() + "] ";
