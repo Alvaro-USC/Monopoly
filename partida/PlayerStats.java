64a6a080ca884ed893e497d269802cd6bd48a13c
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-05 19:32:13 +0100

    Arreglado el formatting del código

--- partida/PlayerStats.java
+++ partida/PlayerStats.java
@@ -12,20 +12,32 @@ public class PlayerStats {
     private float premiosBote = 0f;
     private int vecesEnLaCarcel = 0;
 
-    public void addDineroInvertido(float v) { dineroInvertido += v; }
-    public void addPagoTasas(float v) { pagoTasasEImpuestos += v; }
-    public void addPagoAlquiler(float v) { pagoDeAlquileres += v; }
-    public void addCobroAlquiler(float v) { cobroDeAlquileres += v; }
-    public void addPasarSalida(float v) { pasarPorSalida += v; }
-    public void addPremioBote(float v) { premiosBote += v; }
-    public void addVezEnCarcel() { vecesEnLaCarcel++; }
+    public void addDineroInvertido(float v) {dineroInvertido += v;}
+
+    public void addPagoTasas(float v) {pagoTasasEImpuestos += v;}
+
+    public void addPagoAlquiler(float v) {pagoDeAlquileres += v;}
+
+    public void addCobroAlquiler(float v) {cobroDeAlquileres += v;}
+
+    public void addPasarSalida(float v) {pasarPorSalida += v;}
+
+    public void addPremioBote(float v) {premiosBote += v;}
+
+    public void addVezEnCarcel() {vecesEnLaCarcel++;}
 
     // Getters
-    public float getDineroInvertido() { return dineroInvertido; }
-    public float getPagoTasasEImpuestos() { return pagoTasasEImpuestos; }
-    public float getPagoDeAlquileres() { return pagoDeAlquileres; }
-    public float getCobroDeAlquileres() { return cobroDeAlquileres; }
-    public float getPasarPorSalida() { return pasarPorSalida; }
-    public float getPremiosBote() { return premiosBote; }
-    public int getVecesEnLaCarcel() { return vecesEnLaCarcel; }
+    public float getDineroInvertido() {return dineroInvertido;}
+
+    public float getPagoTasasEImpuestos() {return pagoTasasEImpuestos;}
+
+    public float getPagoDeAlquileres() {return pagoDeAlquileres;}
+
+    public float getCobroDeAlquileres() {return cobroDeAlquileres;}
+
+    public float getPasarPorSalida() {return pasarPorSalida;}
+
+    public float getPremiosBote() {return premiosBote;}
+
+    public int getVecesEnLaCarcel() {return vecesEnLaCarcel;}
 }
