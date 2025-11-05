e58c85db8ce4c1552428bd311f762b09a777a2d8
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-05 19:22:56 +0100

    Ahora al evaluar la casilla lo que hacemos es llamar al método procesarPago de Casilla.java para que haga la operación de sumar/restar el dinero al jugador

--- monopoly/Servicio.java
+++ monopoly/Servicio.java
@@ -1,58 +1,31 @@
 package monopoly;
 
-import partida.*;
-import java.util.ArrayList;
+import partida.Jugador;
 
-public class Servicio extends Casilla {
+public class Servicio extends PropiedadComprable {
     public Servicio(String nombre, int posicion, float valor, Jugador duenho) {
         super(nombre, "Servicios", posicion, valor, duenho);
     }
 
     @Override
     public boolean evaluarCasilla(Jugador actual, Jugador banca, int tirada) {
-        boolean solv = true;
-        if (!getDuenho().equals(banca) && !getDuenho().equals(actual)) {
-            float toPay = 4 * tirada * Valor.FACTOR_SERVICIO;
-            if (actual.getFortuna() < toPay) {
-                solv = false;
-                System.out.println("No tienes suficiente dinero. Debes hipotecar una propiedad o declararte en bancarrota.");
-            } else {
-                actual.sumarFortuna(-toPay);
-                getDuenho().sumarFortuna(toPay);
-                System.out.println("Se han pagado " + Valor.formatear(toPay) + " € de alquiler.");
-                StatsTracker.getInstance().registrarPagoAlquiler(actual, toPay);
-                StatsTracker.getInstance().registrarCobroAlquiler(getDuenho(), toPay);
-                StatsTracker.getInstance().registrarAlquiler(this, toPay);
-            }
-        } else System.out.println("El dueño de esta casilla es " + getDuenho().getNombre() + ", se puede comprar.");
-        return solv;
-    }
-
-    @Override
-    public void comprarCasilla(Jugador solicitante, Jugador banca) {
         if (getDuenho().equals(banca)) {
-            if (solicitante.getFortuna() >= getValor()) {
-                solicitante.sumarFortuna(-getValor());
-                banca.sumarFortuna(getValor());
-                setDuenho(solicitante);
-                solicitante.anhadirPropiedad(this);
-                System.out.println("El jugador " + solicitante.getNombre() + " compra la casilla " + getNombre() + " por " + Valor.formatear(getValor()) + "€. Su fortuna actual es " + Valor.formatear(solicitante.getFortuna()) + "€.");
-            } else {
-                System.out.println("No tienes suficiente dinero para comprar esta casilla.");
-            }
-        } else {
-            System.out.println("Esta casilla no se puede comprar.");
+            System.out.println("Esta casilla (" + getNombre() + ") pertenece a la banca y se puede comprar.");
+            return true;
         }
+
+        if (getDuenho().equals(actual)) {
+            return true;
+        }
+
+        float toPay = 4 * tirada * Valor.FACTOR_SERVICIO;
+
+        return this.procesarPago(actual, toPay);
     }
 
     @Override
-    public String infoCasilla() {
-        String info = "{ \n tipo: " + getTipo() + ", \n propietario: " + getDuenho().getNombre() + ", \n valor: " + Valor.formatear(getValor()) + "\n}";
-        return info;
-    }
+    public String infoCasilla() {return "{ \n tipo: " + getTipo() + ", \n propietario: " + getDuenho().getNombre() + ", \n valor: " + Valor.formatear(getValor()) + "\n}";}
 
     @Override
-    public String casEnVenta() {
-        return "{\n nombre: " + this.getNombre() +  "\n tipo: " + getTipo() + ", \n valor: " + Valor.formatear(getValor()) + "\n}";
-    }
+    public String casEnVenta() {return "{\n nombre: " + this.getNombre() + "\n tipo: " + getTipo() + ", \n valor: " + Valor.formatear(getValor()) + "\n}";}
 }
