a87c46564cd72342f59a3707ea2cdf2024995b7a
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-05 19:18:15 +0100

    Arreglado el formatting del código

--- monopoly/MonopolyETSE.java
+++ monopoly/MonopolyETSE.java
@@ -2,7 +2,7 @@ package monopoly;
 
 public class MonopolyETSE {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         if (args.length > 0) {
             new Menu(args[0]);
         } else new Menu();
