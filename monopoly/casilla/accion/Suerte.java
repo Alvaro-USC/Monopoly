30953c4d64d93bea813d883ea4cf2c8c5549d3c2
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:41:27 +0100

    Borrado el método de comprarCasilla, que ya no es de Casilla, sino de Propiedad, no tiene sentido en Suerte

--- monopoly/casilla/accion/Suerte.java
+++ monopoly/casilla/accion/Suerte.java
@@ -1,20 +1,17 @@
 package monopoly.casilla.accion;
 
-import monopoly.casilla.Casilla;
+import monopoly.casilla.Accion;
 import partida.Jugador;
 
-public class Suerte extends Casilla {
+public class Suerte extends Accion {
     public Suerte(int posicion, Jugador duenho) {
-        super("Suerte", "Suerte", posicion, duenho);
+        super("Suerte", posicion, duenho);
     }
 
     @Override
     public boolean evaluarCasilla(Jugador actual, Jugador banca, int tirada) {
         return true;
     }
-
-    @Override
-    public void comprarCasilla(Jugador solicitante, Jugador banca) {System.out.println("Esta casilla no se puede comprar.");}
 
     @Override
     public String infoCasilla() {return "{ \n tipo: " + getTipo() + "\n}";}
