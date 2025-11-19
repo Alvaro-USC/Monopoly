41a250fbce234464807eef585963eae21501bc7f
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:42:51 +0100

    Borrada la comprobación de que la carcel sea una Casilla null, puesto que en la construcción del Tablero siempre se añade y crea

--- monopoly/casilla/especial/IrCarcel.java
+++ monopoly/casilla/especial/IrCarcel.java
@@ -11,24 +11,17 @@ public class IrCarcel extends Casilla {
     @Override
     public boolean evaluarCasilla(Jugador actual, Jugador banca, int tirada) {
         Casilla carcel = getTablero().encontrar_casilla("Carcel");
-        if (carcel == null) {
-            System.out.println("Error: No se encontró la casilla Carcel.");
-        } else {
-            Casilla lugarAnterior = actual.getAvatar().getLugar();
-            if (lugarAnterior != null) {
-                lugarAnterior.eliminarAvatar(actual.getAvatar());
-            }
-            actual.getAvatar().setLugar(carcel);
-            carcel.anhadirAvatar(actual.getAvatar());
-            actual.setEnCarcel(true);
-            actual.setTiradasCarcel(0);
-            System.out.println("El avatar se coloca en la casilla de Carcel.");
+        Casilla lugarAnterior = actual.getAvatar().getLugar();
+        if (lugarAnterior != null) {
+            lugarAnterior.eliminarAvatar(actual.getAvatar());
         }
+        actual.getAvatar().setLugar(carcel);
+        carcel.anhadirAvatar(actual.getAvatar());
+        actual.setEnCarcel(true);
+        actual.setTiradasCarcel(0);
+        System.out.println("El avatar se coloca en la casilla de Carcel.");
         return true;
     }
-
-    @Override
-    public void comprarCasilla(Jugador solicitante, Jugador banca) {System.out.println("Esta casilla no se puede comprar.");}
 
     @Override
     public String infoCasilla() {return "{ \n tipo: " + getTipo() + "\n}";}
