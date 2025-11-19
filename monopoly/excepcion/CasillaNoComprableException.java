a0bb6e2563a40126119b8ea0536598301ef8443f
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:48:42 +0100

    Creada la clase para generar excepciones generales de Casillas que no son Propiedad

--- monopoly/excepcion/CasillaNoComprableException.java
+++ monopoly/excepcion/CasillaNoComprableException.java
@@ -1,3 +1,8 @@
 package monopoly.excepcion;
 
-public class CasillaNoComprableException {}
+// Casilla no comprable (especiales como Suerte)
+public final class CasillaNoComprableException extends PropiedadException {
+    public CasillaNoComprableException(String mensaje) {
+        super(mensaje);
+    }
+}
