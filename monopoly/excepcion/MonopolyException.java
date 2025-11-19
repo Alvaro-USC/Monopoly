04509a7a1e2b6a7a39611cadce49bcde30485e7e
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:50:38 +0100

    Clase base abstracta para desarrollar los handlers de excepciones

--- monopoly/excepcion/MonopolyException.java
+++ monopoly/excepcion/MonopolyException.java
@@ -1,3 +1,10 @@
 package monopoly.excepcion;
 
-public class MonopolyException {}
+/**
+ * Clase base para todas las excepciones de Monopoly.
+ */
+public abstract class MonopolyException extends Exception {
+    public MonopolyException(String mensaje) {
+        super(mensaje);
+    }
+}
