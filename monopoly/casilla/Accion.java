330d75f93c0dd4816db252e33ef5873e4c42fa34
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:37:33 +0100

    Creada la clase Accion que es una Casilla con tipo Especial

--- monopoly/casilla/Accion.java
+++ monopoly/casilla/Accion.java
@@ -1,4 +1,11 @@
 package monopoly.casilla;
 
-public class Accion {
+import partida.Jugador;
+
+public abstract class Accion extends Casilla {
+
+    public Accion(String nombre, int posicion, Jugador duenho) {
+        super(nombre, "Especial", posicion, duenho);
+    }
+
 }
