8f15566d7d058e3570ff53ca1917799645423d8c
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:49:06 +0100

    Creada la clase para generar excepciones generales de Propiedad que no suple alguno de los requisitos para edificar()

--- monopoly/excepcion/EdificacionIlegalException.java
+++ monopoly/excepcion/EdificacionIlegalException.java
@@ -1,3 +1,12 @@
 package monopoly.excepcion;
 
-public class EdificacionIlegalException {}
+// Edificación ilegal (por ejemplo, construir hotel sin tener casas)
+public final class EdificacionIlegalException extends PropiedadException {
+    public EdificacionIlegalException() {
+        super("No se puede edificar en este grupo mientras haya propiedades hipotecadas.");
+    }
+
+    public EdificacionIlegalException(String mensaje) {
+        super(mensaje);
+    }
+}
