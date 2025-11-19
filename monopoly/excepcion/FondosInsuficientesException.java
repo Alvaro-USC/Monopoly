72e97feeef3ee06dbd0d4d7abdfa572a8b9eb84b
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:49:55 +0100

    Creada la clase para generar excepciones generales cuando un jugador no tiene dinero suficiente para acometer un pago

--- monopoly/excepcion/FondosInsuficientesException.java
+++ monopoly/excepcion/FondosInsuficientesException.java
@@ -1,3 +1,14 @@
 package monopoly.excepcion;
 
-public class FondosInsuficientesException {}
+import monopoly.Valor;
+
+// Fondos insuficientes
+public final class FondosInsuficientesException extends AccionInvalidaException {
+    public FondosInsuficientesException(float cantidad) {
+        super("No tienes suficiente dinero (" + Valor.formatear(cantidad) + "€) para deshipotecar esta propiedad.");
+    }
+
+    public FondosInsuficientesException(String nombreCasilla) {
+        super("No tienes suficiente dinero para comprar / edificar " + nombreCasilla);
+    }
+}
