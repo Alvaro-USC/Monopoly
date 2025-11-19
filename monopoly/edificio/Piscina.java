2ebbaef6427cb703f909bdecf3c8e67450d88483
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:47:26 +0100

    Creada la subclase Piscina que extiende la abstracta Edificio solo especificando su tipo piscina

--- monopoly/edificio/Piscina.java
+++ monopoly/edificio/Piscina.java
@@ -1,3 +1,10 @@
 package monopoly.edificio;
 
-public class Piscina {}
+import monopoly.casilla.propiedad.Solar;
+import partida.Jugador;
+
+public final class Piscina extends Edificio {
+    public Piscina(Jugador propietario, Solar solar, String grupo, float coste) {
+        super("piscina", propietario, solar, grupo, coste);
+    }
+}
