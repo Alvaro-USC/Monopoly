966980ebd39e64d44c049827255f3280b3b9606e
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:30:10 +0100

    Implementacion de la consola con el Scanner de la libstd y el System.out

--- monopoly/ConsolaNormal.java
+++ monopoly/ConsolaNormal.java
@@ -1,3 +1,24 @@
 package monopoly;
 
-public class ConsolaNormal {}
+import java.util.Scanner;
+
+public class ConsolaNormal implements Consola {
+    private final Scanner scanner;
+
+    public ConsolaNormal() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public void imprimir(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    @Override
+    public String leer(String descripcion) {
+        if (descripcion != null && !descripcion.isEmpty()) {
+            System.out.print(descripcion + " "); // Añadimos un espacio para estética
+        }
+        return scanner.nextLine().trim();
+    }
+}
