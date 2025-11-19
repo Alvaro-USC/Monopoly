17984445fb58e9b94595246c6377a177f0d3cd72
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:47:45 +0100

    Creada la subclase PistaDeporte que extiende la abstracta Edificio solo especificando su tipo pista_deporte

--- monopoly/edificio/PistaDeporte.java
+++ monopoly/edificio/PistaDeporte.java
@@ -1,3 +1,10 @@
 package monopoly.edificio;
 
-public class PistaDeporte {}
+import monopoly.casilla.propiedad.Solar;
+import partida.Jugador;
+
+public final class PistaDeporte extends Edificio {
+    public PistaDeporte(Jugador propietario, Solar solar, String grupo, float coste) {
+        super("pista_deporte", propietario, solar, grupo, coste);
+    }
+}
