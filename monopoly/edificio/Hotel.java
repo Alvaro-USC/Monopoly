5036275300ea562d2a9c4dbf05390a1e72f2da5c
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:47:11 +0100

    Creada la subclase Hotel que extiende la abstracta Edificio solo especificando su tipo hotel

--- monopoly/edificio/Hotel.java
+++ monopoly/edificio/Hotel.java
@@ -1,3 +1,10 @@
 package monopoly.edificio;
 
-public class Hotel {}
+import monopoly.casilla.propiedad.Solar;
+import partida.Jugador;
+
+public final class Hotel extends Edificio {
+    public Hotel(Jugador propietario, Solar solar, String grupo, float coste) {
+        super("hotel", propietario, solar, grupo, coste);
+    }
+}
