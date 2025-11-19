6208478079520b9c3869d916424bee0cfd1bbe15
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-19 21:51:22 +0100

    Clase abstracta para desarrollar las excepciones relacionadas con la propiedad

--- monopoly/excepcion/PropiedadException.java
+++ monopoly/excepcion/PropiedadException.java
@@ -1,4 +1,10 @@
 package monopoly.excepcion;
 
-public class PropiedadException {
+import monopoly.excepcion.MonopolyException;
+
+// Errores relacionados con propiedades y edificios
+public abstract class PropiedadException extends MonopolyException {
+    public PropiedadException(String mensaje) {
+        super(mensaje);
+    }
 }
