53f6abf8c796e73d53ce7960a0e04de92e26b154
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-20 17:33:39 +0100

    Modificada la función main() para que sea public static void, como Dios manda

--- monopoly/MonopolyETSE.java
+++ monopoly/MonopolyETSE.java
@@ -2,7 +2,7 @@ package monopoly;
 
 public class MonopolyETSE {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         if (args.length > 0) {
             new Juego(args[0]);
         } else new Juego();
