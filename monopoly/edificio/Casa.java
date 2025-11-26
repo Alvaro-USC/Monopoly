49e833e4a528f93435902f6783cab65a2cfbe2bf
Nicolás S. Gómez Forján <nicogomfor@gmail.com>
2025-11-26 12:28:25 +0100

    Refractorizar o código para que generarId sexa delegado ás subclases

--- monopoly/edificio/Casa.java
+++ monopoly/edificio/Casa.java
@@ -7,4 +7,8 @@ public final class Casa extends Edificio {
     public Casa(Solar solar, String grupo, float coste) {
         super("casa", solar, grupo, coste);
     }
+
+    protected String generarId(String tipo) {
+        return "casa-" + (this.getSolar().getCantidadEdificioTipo("casa") + 1);
+    }
 }
