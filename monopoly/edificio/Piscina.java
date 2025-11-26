ca8299b037b4ccc4709c3af37e824543a108439b
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2025-11-26 12:18:38 +0100

    Eliminado el import innecesario de Jugador en las clases de edificio

--- monopoly/edificio/Piscina.java
+++ monopoly/edificio/Piscina.java
@@ -1,7 +1,6 @@
 package monopoly.edificio;
 
 import monopoly.casilla.propiedad.Solar;
-import partida.Jugador;
 
 public final class Piscina extends Edificio {
     public Piscina(Solar solar, String grupo, float coste) {
--- monopoly/edificio/PistaDeporte.java
+++ monopoly/edificio/PistaDeporte.java
@@ -1,7 +1,6 @@
 package monopoly.edificio;
 
 import monopoly.casilla.propiedad.Solar;
-import partida.Jugador;
 
 public final class PistaDeporte extends Edificio {
     public PistaDeporte(Solar solar, String grupo, float coste) {
